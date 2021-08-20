c10bcb3272dbe52d15941338fc533484285a0628
j1mmyson <bwson98@gmail.com>
2021-08-20 15:26:49 +0900

    FIX 05_Module

--- 05_Module/myModule.js
+++ 05_Module/myModule.js
@@ -2,19 +2,13 @@ const path = require('path');
 const fs = require('fs')
 
 module.exports = function (dirPath, ext, callback) {
-
     fs.readdir(dirPath, function(err, list){
         if(err){
             return callback(err)
         }
-        
         list = list.filter(function(item){
             return path.extname(item).slice(1) === ext
         })
-
         callback(null, list)
     })
-
 }
-
-
